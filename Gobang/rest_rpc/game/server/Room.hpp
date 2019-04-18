f2ff87599af5c4f9cda1f6021622f3ad7af680ad
Winter-Win <669886902@qq.com>
2019-04-18 20:45:13 +0800

    Romm Begin

--- Gobang/rest_rpc/game/server/Room.hpp
+++ Gobang/rest_rpc/game/server/Room.hpp
@@ -8,7 +8,28 @@
 class Room
 {
 private:
+    uint32_t one;
+    uint32_t two;
+    char piece[2];
+    uint32_t current;
+    char board[SIZE][SIZE];
+    char result; // X, O, E, N
 
+    pthread_mutex_t lock;
 
+public:
+    Room(uint32_t &id1, uint32_t &id2):one(id1),two(id2)
+    {
+        piece[0] = "X";
+        piece[1] = "O";
+        memset(board, " ", sizeof(board));
+        result = "N";
+        pthread_mutex_init(&lock);
+    }
+
+    ~Room()
+    {
+        pthread_mutex_destroy(&lock);
+    }
 };
-            areateRoom(play_one, play_two);
+            createRoom(play_one, play_two);
